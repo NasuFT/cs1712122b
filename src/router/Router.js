0a99679be626a91bde3a6ab25952fe48dc1300c8
Josh Merlin <jmerlin.suf@gmail.com>
2022-05-17 21:30:32 +0800

    router change fix 2

--- src/router/Router.js
+++ src/router/Router.js
@@ -6,8 +6,8 @@ import Home from "../components/LSystem/Home";
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate replace to="/lsystem" />} />
-      <Route path="/lsystem" element={<Home />} />
+      {/* <Route path="/" element={<Navigate replace to="/lsystem" />} /> */}
+      <Route path="/" element={<Home />} />
       {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
     </Routes>
   );
